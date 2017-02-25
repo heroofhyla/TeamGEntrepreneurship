77099fd18203c541cadf4e557e3285dedbd5208b
rjgreen303 <rjgreen@asu.edu>
2017-02-25 14:16:04 -0700

    Added Space Class

---
+++ Entrepreneurship/src/teamg/entrepreneurship/Space.java
@@ -0,0 +1,35 @@
+package teamg.entrepren	eurship;
+
+import java.awt.Color;
+import java.util.List;
+
+public class Space {
+	private List<Space> nextSpaces;
+	private Runnable effect;
+	private Color color;
+	
+	public Space (List<Space> nextSpaces, Runnable effect){
+		this.setNextSpaces(nextSpaces);
+		this.effect = effect;
+	}
+	
+	public List<Space> getNextSpaces() {
+		return nextSpaces;
+	}
+	
+	public void setNextSpaces(List<Space> nextSpaces) {
+		this.nextSpaces = nextSpaces;
+	}
+	public void activate(){
+		effect.run();
+	}
+
+	public Color getColor() {
+		return color;
+	}
+
+	public void setColor(Color color) {
+		this.color = color;
+	}
+}
+
